3685b1d64399fe16e624774c5d8c9ca42b2bab57
K Ernest (iFire) Lee <ernest.lee@chibifire.com>
2013-12-09 19:43:26 -0800

    Initial commit.

---
+++ Test.hs
@@ -0,0 +1,30 @@
+module Main where
+
+import Graphics.UI.SDL.Video
+import Graphics.UI.SDL.General
+import Graphics.UI.SDL.Types
+import Graphics.UI.SDL.Events
+
+
+import Foreign
+import Data.Typeable
+import Data.Char
+
+import System.Environment
+import System.Exit
+import System.Random
+
+width = 640
+height = 480
+posWidth = 100
+posHeight = 100
+
+main = withInit [InitVideo] $ 
+    do window <- createWindow "Test" (Position posWidth posHeight) (Size width height) [WindowOpengl]
+       render <- createRenderer window FirstSupported [Software, PresentVSync] 
+       image <- loadBMP "image.bmp"
+       texture <- createTextureFromSurface render image
+       renderClear render
+       renderCopy render texture Nothing Nothing
+       renderPresent render
+
